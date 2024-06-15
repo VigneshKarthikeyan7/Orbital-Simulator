70feb155b893b76bce862f83d10c2eea0f73cb6b
c0dy1o <68795433+c0dy1o@users.noreply.github.com>
2024-06-15 14:47:15 -0400

    Install numpy addition

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # Main set of code that we will use to run everything
-
+import numpy as np
 
 def main():
     # Main function
     print("Starting Main Function")
+    initial_position = np.array([7000,0,0])
+    
 
 
 if __name__ == '__main__':
